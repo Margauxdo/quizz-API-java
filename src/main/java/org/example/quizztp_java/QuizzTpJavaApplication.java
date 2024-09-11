cc4c3821d198c0dd14e0fab28631f8b826a55e39
Margaux Doisy <margauxdoisy@gmail.com>
2024-09-11 11:01:19 +0200

    initial commit

---
+++ src/main/java/org/example/quizztp_java/QuizzTpJavaApplication.java
@@ -0,0 +1,13 @@
+package org.example.quizztp_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzTpJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizzTpJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/quizztp_java/QuizzTpJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.quizztp_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzTpJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
